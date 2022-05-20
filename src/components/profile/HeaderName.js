b737e535bad8cce644cbf005dba2c2f70600c36e
gwangmiiin <0424kgm@gmail.com>
2022-05-20 20:44:47 +0900

    볼거리 수정(장소이름 추가)

---
+++ src/components/profile/HeaderName.js
@@ -0,0 +1,11 @@
+import H2 from '@material-tailwind/react/Heading2';
+
+export default function HeaderName() {
+    return (
+        <section className="relative block h-[500px]">
+            <div className="background-profile bg-cover bg-center absolute top-0 w-full h-full">
+                <H2 className = "" color="white">종로</H2>
+            </div>
+        </section>
+    );
+}
---
+++ src/components/profile/HeaderTop.js
@@ -0,0 +1,7 @@
+export default function HeaderTop() {
+    return (
+        <section className="relative block h-[100px]">
+            <div className="bg-cover bg-center absolute top-0 w-full h-full" style={{backgroundColor : "gray"}} />
+        </section>
+    );
+}
