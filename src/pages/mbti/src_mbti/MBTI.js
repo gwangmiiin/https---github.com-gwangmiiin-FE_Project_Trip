e6523077c7312906dff2d3467c606a11f173365e
gwangmiiin <0424kgm@gmail.com>
2022-05-27 19:23:41 +0900

    mbti추가

---
+++ src/pages/mbti/src_mbti/MBTI.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './pages/Home';
+import Question from './pages/Question';
+import Result from './pages/Result';
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/qustion' element={<Question />} />
+        <Route path='/result' element={ <Result />} />      
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/mbti/src_mbti/asset/data/qustionData.js
@@ -0,0 +1,95 @@
+export const QuestionData = [
+    {
+      id: 1,
+      title: "여행을 떠날때 계획은?",
+      answera: "내가 걷는 길이 곧 여행코스.",
+      answerb: "계획은 필수.",
+      type: "EI",
+    },
+    {
+      id: 2,
+      title: "여행 경비는?",
+      answera:
+        "당장 거지만 안되면 되지!",
+      answerb:
+        "걸어다니는 계산기",
+      type: "EI",
+    },
+    {
+      id: 3,
+      title: "여행을 다녀온 후",
+      answera:
+        "침대로 점프!",
+      answerb: "캐리어를 열고 물건을 정리한다.",
+      type: "EI",
+    },
+    {
+      id: 4,
+      title: "여행지에서 식사할 때",
+      answera: "유~명한 맛집을 작정하고 노리는 헌터",
+      answerb: "처음 본 순간 사랑에 빠진 길거리 가게",
+      type: "SN",
+    },
+    {
+      id: 5,
+      title:
+        "여행지에서 길을 잃었을 때",
+      answera: "왔던 길로 돌아가는 헨젤과 그레텔st",
+      answerb: "자꾸 걸어 나가면 길이 있겠지 지구는 둥그니까",
+      type: "SN",
+    },
+    {
+      id: 6,
+      title: "화려한 건축물을 보며 드는 생각은?",
+      answera: "'어떤 방법으로 지었을까' 고민한다.",
+      answerb: "'와 멋있다...' 감탄한다.",
+      type: "SN",
+    },
+    {
+      id: 7,
+      title: "아침에 늦잠을 잔 친구에게",
+      answera:
+        "여행이 역시 피곤하지",
+      answerb: "내일은 시간 지키자",
+      type: "TF",
+    },
+    {
+      id: 8,
+      title: "친구에게 차 사고가 났다고 전화 왔을 때 나의 대답은?",
+      answera:
+        "괜찮아? 다친곳은 없어?",
+      answerb:
+        "보험 들었어?",
+      type: "TF",
+    },
+    {
+      id: 9,
+      title: "친구가 쓸데 없는 기념품을 살 때",
+      answera: "그래 니가 행복하다면...",
+      answerb: "그거 결국 쓰레기 된다.",
+      type: "TF",
+    },
+    {
+      id: 10,
+      title: "나는 여행지를 선택할때 주로",
+      answera:
+        "사람이 많은 도시로",
+      answerb: "나무가 많은 자연으로",
+      type: "JP",
+    },
+    {
+      id: 11,
+      title: "숙소를 구할 때",
+      answera:
+        "저녁에 바베큐 파티를 여는 곳",
+      answerb: "조용하고 아늑한 곳",
+      type: "JP",
+    },
+    {
+      id: 12,
+      title: "여행지에 대한 감상을",
+      answera: "말로 내뱉어야 직성이 풀린다.",
+      answerb: "내 마음 속에 저-장. 마음에 담는다.",
+      type: "JP",
+    },
+  ];
